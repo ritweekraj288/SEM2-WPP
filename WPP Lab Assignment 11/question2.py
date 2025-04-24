138320b8c30d87eacdcefe2fea49198d125b7196
ritweekraj288 <u24ai067@aid.svnit.ac.in>
2025-04-24 17:24:59 +0530

    Delete WPP Lab Assignment 11 directory

--- WPP Lab Assignment 11/question2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-s = pd.Series (["X","Y", "T", "Aaba", "Baca", "CABA", None, "bird", "horse", "dog"])
-a=s.str.upper()
-print("uppercase: ",a)
-b=s.str.lower()
-print("lowercase: ",b)
-c=s.str.len()
-print("length: ",c)
-
--- WPP Lab Assignment 11/question3.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-asking_prices = pd.Series([15000, 18000, 20000, 17000, 22000])fair_prices = pd.Series([16000, 17500, 21000, 18000, 22500])
-good_deals = asking_prices[asking_prices < fair_prices].index.tolist()
-print("Indices of good deals:", good_deals)
-
